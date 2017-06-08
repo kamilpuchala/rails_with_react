ef0ba64754f3a0554d3f5e3bb8a5b6e0a582b946
kamilpuchala <kamilpuchalaa@gmail.com>
2017-06-08 16:09:18 +0200

    add sort column

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,8 +3,8 @@ module Api
 		before_action :set_event, only: [:update, :destroy]
 
 		def index
-			render json: Event.all
-		end
+      render json: Event.order(sort_by + ' ' + order)
+    end
 
 		def create
 			event = Event.new(event_params)
@@ -44,5 +44,16 @@ module Api
 		def set_event
 			@event = Event.find(params[:id])
 		end
+
+		def sort_by
+      %w(name
+         place
+         description
+         event_date).include?(params[:sort_by]) ? params[:sort_by] : 'name'
+    end
+
+    def order
+      %w(asc desc).include?(params[:order]) ? params[:order] : 'asc'
+    end
 	end
 end
